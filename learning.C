9bc7752801311f209e464f55d2813c4457dec514
Muhammad Ismail Shakir <160407658+MuhammadIsmailShakir2009@users.noreply.github.com>
2025-02-24 21:37:24 +0500

    Add files via upload

---
+++ learning.C
@@ -0,0 +1,70 @@
+/// PERCENTAGE  OF 15 STDENTS 
+#iclude<stdio.h>
+int main(){
+    int sub,marks,sum,i,j;
+    flot p;
+    for(i=1;1<=15;i++){
+        scanf("%d",&sub);
+        sum=0;
+        for(j=1;j<=sub;j++){
+            scanf("%d",&marks);
+            sum=sum+marks;
+        }
+        p=(sum*100.0)/(75*sub);
+        printf("%f";p);
+    }
+}
+return 0;
+
+
+///  OUTPUT tHE CCOUNT OF VOWELS THAT APPERS IIN A STRING
+#iclude<stdio.h>
+int main(){
+    int count =0,i;
+    chars[30];
+    gets(s);
+    for(i=0;i<=29;i++){
+        if 
+    }
+}
+
+
+/// ARMSTRONG
+#iclude<stdio.h>
+#include<math.h>
+int main(){
+    int d,n,t,s=0,i;
+    scanf("%d",&n);
+    t=n;
+    d=log10(n)+1;
+    for(i=1;i<-d;i++){
+        s=s+pow(n%10,d);
+        n=n/10;
+    }
+    if(t==s)
+    printf("armstrong");
+else printf("not armstrong");
+}
+
+
+/// CONCATENATE TWO STRING 
+#iclude<stdio.h>
+int main(){
+    char a [8],b [5],c [13];
+    int i,j;
+    gets(a);
+    gets(b);
+    for(i=0,i<8;i++){
+        c [i] = q [i];  
+
+    }
+    for (j=0;j<5;j++)
+    {
+        c[i]=b[j];
+    }
+    i++;
+}
+
+/// reverse A STRING
+#iclude<stdio.h>
+int main(){}
